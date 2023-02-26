d07c1cb0a7489e197af2b8dcce9d1e6e73833874
Alexander <rold_teem_dfc@mail.ru>
2023-02-26 08:18:17 +0300

    STFU

---
+++ AttendanceView.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+import HW5.data.Student;
+
+public class AttendanceView {
+    public void print(List<Student> listStudents) {
+        for (Object object : listStudents) {
+            System.out.println(object);
+        }
+    }
+}
